5feee8b7f7d82456074118471ca2fcbb64cf0b5a
André Rodrigues <andreluizchr1@gmail.com>
2021-04-15 12:53:08 -0300

    Projeto Básico JSF

---
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -0,0 +1,18 @@
+package br.com.caelum.livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+
+	private Livro livro = new Livro();
+
+	public void gravar() {
+		System.out.println("Gravou " + this.livro.getTitulo() );
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+}
